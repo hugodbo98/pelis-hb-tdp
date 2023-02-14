6f741c256c58b5425d1da83efeb4df661727054f
hugodbo98 <124537924+hugodbo98@users.noreply.github.com>
2023-02-14 20:56:48 -0300

    Update BarraSup.jsx

--- src/componentes/BarraSup.jsx
+++ src/componentes/BarraSup.jsx
@@ -3,23 +3,34 @@ import './BarraSup.css'
 import { Link } from "react-router-dom";
 
 export default function BarraSup() {
+  const accionSalida= () => {
+    window.localStorage.removeItem("estaConectado");
+    window.localStorage.removeItem("nombreOnline");
+    window.localStorage.removeItem("apellidoOnline");
+    window.location.reload();
+  }
   return (
     <div className='ctn-barraSup'>
       <div className='links-barraSup'>
-        <Link to= '/' style={{ textDecoration:'none'}}>Inicio</Link>
+        <Link to= '/' style={{ textDecoration:'none'}}><a>Pantalla Inicial</a></Link>
       </div>
       <div className='logo-barraSup'>
         PelisHB
       </div>
       {(window.localStorage.getItem("estaConectado")) && 
-        <div className='status-barraSup'>
-          <p>Conectado</p>
-          {window.localStorage.getItem("nombreOnline")}
-          {window.localStorage.getItem("apellidoOnline")}
+        <div className='der-barraSup'>
+          <div className='status-barraSup'>
+            <p>Conectado</p>
+            {window.localStorage.getItem("nombreOnline")}
+            {window.localStorage.getItem("apellidoOnline")}
+          </div>
+          <div className='salir-barraSup'>
+            <p onClick={accionSalida}>(Salir)</p>
+          </div>
         </div>
       }
       {!(window.localStorage.getItem("estaConectado")) && 
-        <div className='status-barraSup'></div>
+        <div className='der-barraSup'></div>
       }
     </div>
   )
